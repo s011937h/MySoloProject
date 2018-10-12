ac8a6869ff02314f3c7d91fafb1e4eb556e8d446
Lauren <s011937h@student.staffs.ac.uk>
2018-10-12 16:33:10 +0100

    Initial Commit

---
+++ Source/MySoloProject/MySoloProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MySoloProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMySoloProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMySoloProjectGameMode();
+};
+
+
+
